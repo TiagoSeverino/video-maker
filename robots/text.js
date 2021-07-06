3dddea23ee60a844296e5547fd79810bab39a07c
TiagoSeverino <tiago@severino.xyz>
2021-07-07 00:55:19 +0100

    Get direct wiki text, remove algorithmia

--- robots/text.js
+++ robots/text.js
@@ -1,9 +1,12 @@
-const algorithmia = require('algorithmia')
-const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
 const watsonApiKey = require('../credentials/watson-nlu.json').apikey
-const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const superAgent =  require('superagent');
+const readline = require('readline-sync');
+const unidecode = require('unidecode');
 
 const state = require('./state.js')
 
@@ -12,11 +15,11 @@ async function robot() {
   const content = state.load()
 
   const nlu = new NaturalLanguageUnderstandingV1({
-    iam_apikey: watsonApiKey,
+    authenticator: new IamAuthenticator({ apikey: watsonApiKey }),
     version: '2018-04-05',
-    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/',
+    serviceUrl: 'https://api.eu-de.natural-language-understanding.watson.cloud.ibm.com/instances/454a16df-6dfd-40cc-bcf0-1f9e82dacdc8',
     language: content.lang
-  })
+  });
 
   await fetchContentFromWikipedia(content)
   sanitizeContent(content)
@@ -28,16 +31,156 @@ async function robot() {
 
   async function fetchContentFromWikipedia(content) {
     console.log('> [text-robot] Fetching content from Wikipedia')
-    const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
-    const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
-    const wikipediaResponse = await wikipediaAlgorithm.pipe({
-      "lang" : content.lang,
-      "articleName": content.searchTerm
-    })
-    const wikipediaContent = wikipediaResponse.get()
+    
+    const images = [];
+    var ctn = '';
+    var title = '';
+    var summary =''
+    var pageid = '';
+    var url = '';
+    const links = [];
+    const references = [];
 
-    content.sourceContentOriginal = wikipediaContent.content
+
+    console.log('Fetching from Wikipedia...')
+    var RealText = await getRealText(content.searchTerm)
+    title = RealText;
+    console.log('Searching content...')
+    await getContent();
+    
+    
+    content.sourceContentOriginal = ctn
     console.log('> [text-robot] Fetching done!')
+
+    //console.log('Building Structure to others Robots...')
+    //return await buildStructure();
+    
+    /*
+    *
+    * Tenta buscar o termo na Wikipedia, se o mesmo não for encontrado ele encerra o programa,
+    * Caso encontre mais de um,o mesmo sugere e pergunta qual termo você realmente tem interesse baseado na busca no Wikipedia.
+    * 
+    * Obs: Um fato interessante é que o mesmo caso mude a URL da wikipedia para https://pt.wiki... o mesmo vai retornar sua busca em Português Brasil
+    */
+    async function getRealText(text){
+        const res = await superAgent.get('https://en.wikipedia.org/w/api.php').query({
+            'action':'opensearch',
+            'search':''+text,
+            'limit':5,
+            'namespace':0,
+            'format':"json"
+        })
+        if(res.body[1].length == 0){
+            console.log('Your search term don\'t return any result')
+            console.log('Tip: Search your therm in English or pre-search valid Words')
+            console.log('Exiting Program...')
+            process.exit()
+        }
+        let sugestions = []
+        res.body[1].forEach(e => {
+            sugestions.push(unidecode(e))
+        });
+        let index = await selectTerm(sugestions)
+        if(index == -1){
+            console.log('You don\'t selected any key')
+            console.log('Exiting Program...')
+            process.exit()
+        }
+        url = res.body[3][index]
+        return res.body[1][index]
+    }
+    async function selectTerm(prefix){
+        return readline.keyInSelect(prefix,'Choose if any of these keys is the desired search :')
+    }
+    /*
+    *
+    * Busca Todas as Informações da Pagina da Wikipedia Conforme a API do Algotithmia, trazendo ate alguns dados a mais, sendo que no momento, não estamos utilizando.
+    * 
+    */
+    async function getContent(){
+        const ret = await superAgent.get('https://en.wikipedia.org/w/api.php').query({
+            'action':'query',
+            'prop': 'extracts|images|links|info|extlinks',
+            'redirects': 1,
+            'exsectionformat':'wiki',
+            'explaintext':true,
+            'titles':RealText,
+            'format':"json"
+        })
+        let value
+        map = new Map(Object.entries(ret.body.query.pages));
+        map.forEach(function(e){
+            value = e;
+        });
+        try{
+            value.links.forEach(e => {
+                links.push(e.title)
+            });
+        }catch(Ex){
+            console.log('----------------------------')
+            console.log('Any Links in this search')
+            console.log('----------------------------')
+        }
+        try{
+            value.extlinks.forEach(e => {
+                references.push(e['*'])
+            });
+        }catch(Ex){
+            console.log('----------------------------')
+            console.log('Any Reference in this search')
+            console.log('----------------------------')
+        }
+        pageid = value.pageid;
+        ctn = value.extract;
+        summary =  value.extract.split('\n\n\n')[0]
+        console.log("Fetching Images...")
+        for (let i = 0; i < value.images.length; i++) {
+            await getURLImage(value.images[i].title);
+        }
+        
+    }
+    /*
+    *
+    * Busca a URL das imagens retornadas anteriormente no metodo getContent(), podendo ser utilizada futuramente em outros robos.
+    * 
+    */
+    async function getURLImage(title){
+        const ret = await superAgent.get('https://en.wikipedia.org/w/api.php').query({
+            'action':'query',
+            'prop': 'imageinfo',
+            'titles':title,
+            'format':"json",
+            'iiprop':'url'
+        })
+        values = [];
+        map = new Map(Object.entries(ret.body.query.pages));
+        map.forEach(function(e){
+            e.imageinfo.forEach(function(e){
+              values.push(e.url)
+            });
+        });
+        values.forEach(function(e){
+          images.push(e);
+        }); 
+    }
+    /*
+    *
+    * Constroi uma estrutura de dados, igual a do Algorithmia.
+    * 
+    */
+    async function buildStructure(){
+        return {
+            content: ctn,
+            images:  images,
+            links: links,
+            pageid:pageid,
+            references:references,
+            summary: summary,
+            title: title,
+            url: url
+        }
+        
+    }
   }
 
   function sanitizeContent(content) {
@@ -86,11 +229,17 @@ async function robot() {
     console.log('> [text-robot] Starting to fetch keywords from Watson')
 
     for (const sentence of content.sentences) {
-      console.log(`> [text-robot] Sentence: "${sentence.text}"`)
+      
+      try {
+        console.log(`> [text-robot] Sentence: "${sentence.text}"`)
 
-      sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
 
-      console.log(`> [text-robot] Keywords: ${sentence.keywords.join(', ')}\n`)
+        console.log(`> [text-robot] Keywords: ${sentence.keywords.join(', ')}\n`)
+      }
+      catch {
+        console.log(`> [text-robot] Failed to process keywords\n`)
+      }
     }
   }
 
@@ -101,21 +250,17 @@ async function robot() {
         features: {
           keywords: {}
         }
-      }, (error, response) => {
-        if (error) {
-          reject(error)
-          return
-        }
-
-        const keywords = response.keywords.map((keyword) => {
+      }).then(response => {
+        const keywords = response.result.keywords.map((keyword) => {
           return keyword.text
         })
 
         resolve(keywords)
-      })
+      }).catch(err => {
+        reject(err)
+      });
     })
   }
-
 }
 
 module.exports = robot
