5b953b309d2091e2eb15548c62217b6201208d50
TiagoSeverino <tiago@severino.xyz>
2021-07-08 12:45:46 +0100

    Add google trends

--- robots/input.js
+++ robots/input.js
@@ -1,34 +1,70 @@
-const readline = require('readline-sync')
-const state = require('./state.js')
+const readline = require("readline-sync");
+const state = require("./state.js");
+const Parser = require("rss-parser");
 
-function robot() {
+async function robot() {
   const content = {
-    maximumSentences: 7
+    maximumSentences: 7,
+  };
+
+  content.searchTerm = await askAndReturnSearchTerm();
+  content.prefix = askAndReturnPrefix();
+  content.lang = askAndReturnLanguage();
+  state.save(content);
+
+  async function askAndReturnSearchTerm() {
+    const response = readline.question(
+      "Type a Wikipedia search term or G to fetch google trends: "
+    );
+
+    return response.toUpperCase() === "G"
+      ? await askAndReturnTrend()
+      : response;
   }
 
-  content.searchTerm = askAndReturnSearchTerm()
-  content.prefix = askAndReturnPrefix()
-  content.lang = askAndReturnLanguage()
-  state.save(content)
+  async function askAndReturnTrend() {
+    const geo = ["BR", "US"];
+    const selectedGeoIndex = readline.keyInSelect(geo, "Choice Trend Geo: ");
+    const selectedGeoText = geo[selectedGeoIndex];
+    content.trendGeo = selectedGeoText;
 
-  function askAndReturnSearchTerm() {
-    return readline.question('Type a Wikipedia search term: ')
+    console.log("Please Wait...");
+    const trends = await getGoogleTrends();
+    const choice = readline.keyInSelect(trends, "Choose your trend:");
+
+    return trends[choice];
+  }
+
+  async function getGoogleTrends() {
+    const TREND_URL =
+      "https://trends.google.com/trends/trendingsearches/daily/rss?geo=" +
+      content.trendGeo;
+
+    const parser = new Parser();
+    const trends = await parser.parseURL(TREND_URL);
+    return trends.items.map(({ title }) => title);
   }
 
   function askAndReturnPrefix() {
-    const prefixes = ['Who is', 'What is', 'The history of']
-    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-    const selectedPrefixText = prefixes[selectedPrefixIndex]
+    const prefixes = ["Who is", "What is", "The history of"];
+    const selectedPrefixIndex = readline.keyInSelect(
+      prefixes,
+      "Choose one option: "
+    );
+    const selectedPrefixText = prefixes[selectedPrefixIndex];
 
-    return selectedPrefixText
+    return selectedPrefixText;
   }
 
-  function askAndReturnLanguage(){
-    const language = ['pt','en', 'es', 'fr']
-    const selectedLangIndex = readline.keyInSelect(language,'Choose Language: ')
-    const selectedLangText = language[selectedLangIndex]
-    return selectedLangText
+  function askAndReturnLanguage() {
+    const language = ["pt", "en", "es", "fr"];
+    const selectedLangIndex = readline.keyInSelect(
+      language,
+      "Choose Language: "
+    );
+    const selectedLangText = language[selectedLangIndex];
+    return selectedLangText;
   }
 }
 
-module.exports = robot
+module.exports = robot;
