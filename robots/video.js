8a80769b794617df6fae573a40e4ec2569db7940
TiagoSeverino <tiago@severino.xyz>
2022-04-19 17:44:30 +0100

    Last update

--- robots/video.js
+++ robots/video.js
@@ -1,271 +1,296 @@
-const gm = require('gm').subClass({imageMagick: true})
-const state = require('./state.js')
-const spawn = require('child_process').spawn
-const path = require('path')
-const rootPath = path.resolve(__dirname, '..')
+const gm = require("gm").subClass({ imageMagick: true });
+const state = require("./state.js");
+const spawn = require("child_process").spawn;
+const path = require("path");
+const rootPath = path.resolve(__dirname, "..");
 
-const os = require('os')
-const fs = require('fs')
+const os = require("os");
+const fs = require("fs");
 
-const videoshow = require('videoshow')
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
-const ffprobePath = require('@ffprobe-installer/ffprobe').path
+const videoshow = require("videoshow");
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const ffprobePath = require("@ffprobe-installer/ffprobe").path;
 
-const audio = path.join(__dirname, '../templates/1/newsroom.mp3')
-const video = path.join(__dirname, '../content/output.mp4')
+const audio = path.join(__dirname, "../templates/1/newsroom.mp3");
+const video = path.join(__dirname, "../content/output.mp4");
 
-let ffmpeg = require('fluent-ffmpeg')
-ffmpeg.setFfmpegPath(ffmpegPath)
-ffmpeg.setFfprobePath(ffprobePath)
+let ffmpeg = require("fluent-ffmpeg");
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
 
-const fromRoot = relPath => path.resolve(rootPath, relPath)
+const fromRoot = (relPath) => path.resolve(rootPath, relPath);
 
 async function robot() {
-  console.log('> [video-robot] Starting...')
-  const content = state.load()
+	console.log("> [video-robot] Starting...");
+	const content = state.load();
 
-  await convertAllImages(content)
-  await createAllSentenceImages(content)
-  await createYouTubeThumbnail()
-  await createVideoScript(content)
-  await renderVideo(content)
+	await convertAllImages(content);
+	await createAllSentenceImages(content);
+	await createYouTubeThumbnail();
+	await createVideoScript(content);
+	await renderVideo(content);
 
-  state.save(content)
+	state.save(content);
 
-  async function convertAllImages(content) {
-    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
-      await convertImage(sentenceIndex)
-    }
-  }
+	async function convertAllImages(content) {
+		for (
+			let sentenceIndex = 0;
+			sentenceIndex < content.sentences.length;
+			sentenceIndex++
+		) {
+			await convertImage(sentenceIndex);
+		}
+	}
 
-  async function convertImage(sentenceIndex) {
-    return new Promise((resolve, reject) => {
-      const inputFile = fromRoot(`./content/${sentenceIndex}-original.png[0]`)
-      const outputFile = fromRoot(`./content/${sentenceIndex}-converted.png`)
-      const width = 1920
-      const height = 1080
+	async function convertImage(sentenceIndex) {
+		return new Promise((resolve, reject) => {
+			const inputFile = fromRoot(
+				`./content/${sentenceIndex}-original.png[0]`
+			);
+			const outputFile = fromRoot(
+				`./content/${sentenceIndex}-converted.png`
+			);
+			const width = 1080;
+			const height = 1920;
 
-      gm()
-        .in(inputFile)
-        .out('(')
-          .out('-clone')
-          .out('0')
-          .out('-background', 'white')
-          .out('-blur', '0x9')
-          .out('-resize', `${width}x${height}^`)
-        .out(')')
-        .out('(')
-          .out('-clone')
-          .out('0')
-          .out('-background', 'white')
-          .out('-resize', `${width}x${height}`)
-        .out(')')
-        .out('-delete', '0')
-        .out('-gravity', 'center')
-        .out('-compose', 'over')
-        .out('-composite')
-        .out('-extent', `${width}x${height}`)
-        .write(outputFile, (error) => {
-          if (error) {
-            return reject(error)
-          }
+			gm()
+				.in(inputFile)
+				.out("(")
+				.out("-clone")
+				.out("0")
+				.out("-background", "white")
+				.out("-blur", "0x9")
+				.out("-resize", `${width}x${height}^`)
+				.out(")")
+				.out("(")
+				.out("-clone")
+				.out("0")
+				.out("-background", "white")
+				.out("-resize", `${width}x${height}`)
+				.out(")")
+				.out("-delete", "0")
+				.out("-gravity", "center")
+				.out("-compose", "over")
+				.out("-composite")
+				.out("-extent", `${width}x${height}`)
+				.write(outputFile, (error) => {
+					if (error) {
+						return reject(error);
+					}
 
-          console.log(`> [video-robot] Image converted: ${outputFile}`)
-          resolve()
-        })
+					console.log(
+						`> [video-robot] Image converted: ${outputFile}`
+					);
+					resolve();
+				});
+		});
+	}
 
-    })
-  }
+	async function createAllSentenceImages(content) {
+		if (os.platform != "darwin" || os.platform != "win32") return;
 
-  async function createAllSentenceImages(content) {
-    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
-      await createSentenceImage(sentenceIndex, content.sentences[sentenceIndex].text)
-    }
-  }
+		for (
+			let sentenceIndex = 0;
+			sentenceIndex < content.sentences.length;
+			sentenceIndex++
+		) {
+			await createSentenceImage(
+				sentenceIndex,
+				content.sentences[sentenceIndex].text
+			);
+		}
+	}
 
-  async function createSentenceImage(sentenceIndex, sentenceText) {
-    return new Promise((resolve, reject) => {
-      const outputFile = fromRoot(`./content/${sentenceIndex}-sentence.png`)
+	async function createSentenceImage(sentenceIndex, sentenceText) {
+		return new Promise((resolve, reject) => {
+			const outputFile = fromRoot(
+				`./content/${sentenceIndex}-sentence.png`
+			);
 
-      const templateSettings = {
-        0: {
-          size: '1920x400',
-          gravity: 'center'
-        },
-        1: {
-          size: '1920x1080',
-          gravity: 'center'
-        },
-        2: {
-          size: '800x1080',
-          gravity: 'west'
-        },
-        3: {
-          size: '1920x400',
-          gravity: 'center'
-        },
-        4: {
-          size: '1920x1080',
-          gravity: 'center'
-        },
-        5: {
-          size: '800x1080',
-          gravity: 'west'
-        },
-        6: {
-          size: '1920x400',
-          gravity: 'center'
-        }
+			const templateSettings = {
+				0: {
+					size: "1920x400",
+					gravity: "center",
+				},
+				1: {
+					size: "1920x1080",
+					gravity: "center",
+				},
+				2: {
+					size: "800x1080",
+					gravity: "west",
+				},
+				3: {
+					size: "1920x400",
+					gravity: "center",
+				},
+				4: {
+					size: "1920x1080",
+					gravity: "center",
+				},
+				5: {
+					size: "800x1080",
+					gravity: "west",
+				},
+				6: {
+					size: "1920x400",
+					gravity: "center",
+				},
+			};
 
-      }
+			gm()
+				.out("-size", templateSettings[sentenceIndex].size)
+				.out("-gravity", templateSettings[sentenceIndex].gravity)
+				.out("-background", "transparent")
+				.out("-fill", "white")
+				.out("-kerning", "-1")
+				.out(`caption:${sentenceText}`)
+				.write(outputFile, (error) => {
+					if (error) {
+						return reject(error);
+					}
 
-      gm()
-        .out('-size', templateSettings[sentenceIndex].size)
-        .out('-gravity', templateSettings[sentenceIndex].gravity)
-        .out('-background', 'transparent')
-        .out('-fill', 'white')
-        .out('-kerning', '-1')
-        .out(`caption:${sentenceText}`)
-        .write(outputFile, (error) => {
-          if (error) {
-            return reject(error)
-          }
+					console.log(
+						`> [video-robot] Sentence created: ${outputFile}`
+					);
+					resolve();
+				});
+		});
+	}
 
-          console.log(`> [video-robot] Sentence created: ${outputFile}`)
-          resolve()
-        })
-    })
-  }
+	async function createYouTubeThumbnail() {
+		return new Promise((resolve, reject) => {
+			gm()
+				.in(fromRoot("./content/0-converted.png"))
+				.write(fromRoot("./content/youtube-thumbnail.jpg"), (error) => {
+					if (error) {
+						return reject(error);
+					}
 
-  async function createYouTubeThumbnail() {
-    return new Promise((resolve, reject) => {
-      gm()
-        .in(fromRoot('./content/0-converted.png'))
-        .write(fromRoot('./content/youtube-thumbnail.jpg'), (error) => {
-          if (error) {
-            return reject(error)
-          }
+					console.log("> [video-robot] YouTube thumbnail created");
+					resolve();
+				});
+		});
+	}
 
-          console.log('> [video-robot] YouTube thumbnail created')
-          resolve()
-        })
-    })
-  }
+	async function createVideoScript(content) {
+		await state.saveScript(content);
+	}
 
-  async function createVideoScript(content) {
-    await state.saveScript(content)
-  }
+	async function renderVideo(content) {
+		let aerenderFilePath;
 
-  async function renderVideo(content) {
-    let aerenderFilePath;
+		const systemPlatform = os.platform;
+		if (systemPlatform == "darwin") {
+			aerenderFilePath =
+				"/Applications/Adobe After Effects CC 2019/aerender";
+		} else if (systemPlatform == "win32") {
+			aerenderFilePath =
+				"%programfiles%AdobeAdobe After Effects CCArquivos de suporteaerender.exe";
+		}
 
-    const systemPlatform = os.platform
-    if (systemPlatform == 'darwin'){
-      aerenderFilePath = '/Applications/Adobe After Effects CC 2019/aerender'
-    }else if (systemPlatform =='win32'){
-      aerenderFilePath = '%programfiles%\Adobe\Adobe After Effects CC\Arquivos de suporte\aerender.exe'
-    }
+		try {
+			if (fs.existsSync(aerenderFilePath)) {
+				return await renderVideoWithAfterEffects(aerenderFilePath);
+			}
+		} catch (err) {
+			console.error(err);
+		}
 
-    try {
-      if (fs.existsSync(aerenderFilePath)) {
-        return await renderVideoWithAfterEffects(aerenderFilePath)
-      }
-    } catch(err) {
-      console.error(err)
-    }
+		return await renderVideoWithFFmpeg(content);
+	}
 
-    return await renderVideoWithFFmpeg(content)
-  }
+	async function renderVideoWithAfterEffects(aerenderFilePath) {
+		return new Promise((resolve, reject) => {
+			const templateFilePath = fromRoot("./templates/1/template.aep");
+			const destinationFilePath = fromRoot("./content/output.mov");
 
-  async function renderVideoWithAfterEffects(aerenderFilePath) {
-    return new Promise((resolve, reject) => {
-      const templateFilePath = fromRoot('./templates/1/template.aep')
-      const destinationFilePath = fromRoot('./content/output.mov')
+			console.log("> [video-robot] Starting After Effects");
 
-      console.log('> [video-robot] Starting After Effects')
+			const aerender = spawn(aerenderFilePath, [
+				"-comp",
+				"main",
+				"-project",
+				templateFilePath,
+				"-output",
+				destinationFilePath,
+			]);
 
-      const aerender = spawn(aerenderFilePath, [
-        '-comp', 'main',
-        '-project', templateFilePath,
-        '-output', destinationFilePath
-      ])
+			aerender.stdout.on("data", (data) => {
+				process.stdout.write(data);
+			});
 
-      aerender.stdout.on('data', (data) => {
-        process.stdout.write(data)
-      })
+			aerender.on("close", () => {
+				console.log("> [video-robot] After Effects closed");
+				resolve();
+			});
+		});
+	}
 
-      aerender.on('close', () => {
-        console.log('> [video-robot] After Effects closed')
-        resolve()
-      })
-    })
-  }
+	async function renderVideoWithFFmpeg(content) {
+		return new Promise((resolve, reject) => {
+			let images = [];
 
-  async function renderVideoWithFFmpeg(content) {
-    return new Promise((resolve, reject) => {
-      let images = []
+			for (
+				let sentenceIndex = 0;
+				sentenceIndex < content.sentences.length;
+				sentenceIndex++
+			) {
+				images.push({
+					path: `./content/${sentenceIndex}-converted.png`,
+					caption: content.sentences[sentenceIndex].text,
+				});
+			}
 
-      for (
-        let sentenceIndex = 0;
-        sentenceIndex < content.sentences.length;
-        sentenceIndex++
-      ) {
-        images.push({
-          path: `./content/${sentenceIndex}-converted.png`,
-          caption: content.sentences[sentenceIndex].text
-        })
-      }
+			const videoOptions = {
+				fps: 25,
+				loop: 5, // seconds
+				transition: true,
+				transitionDuration: 1, // seconds
+				videoBitrate: 1024,
+				videoCodec: "libx264",
+				size: "1080x1920",
+				audioBitrate: "128k",
+				audioChannels: 1,
+				format: "mp4",
+				pixelFormat: "yuv420p",
+				useSubRipSubtitles: false, // Use ASS/SSA subtitles instead
+				subtitleStyle: {
+					Fontname: "Ubuntu",
+					Fontsize: "26",
+					PrimaryColour: "11861244",
+					SecondaryColour: "11861244",
+					TertiaryColour: "11861244",
+					BackColour: "-2147483640",
+					Bold: "2",
+					Italic: "0",
+					BorderStyle: "2",
+					Outline: "2",
+					Shadow: "3",
+					Alignment: "2", // left, middle, right
+					MarginL: "40",
+					MarginR: "60",
+					MarginV: "40",
+				},
+			};
 
-      const videoOptions = {
-        fps: 25,
-        loop: 5, // seconds
-        transition: true,
-        transitionDuration: 1, // seconds
-        videoBitrate: 1024,
-        videoCodec: "libx264",
-        size: "640x?",
-        audioBitrate: "128k",
-        audioChannels: 2,
-        format: "mp4",
-        pixelFormat: "yuv420p",
-        useSubRipSubtitles: false, // Use ASS/SSA subtitles instead
-        subtitleStyle: {
-          Fontname: "Verdana",
-          Fontsize: "26",
-          PrimaryColour: "11861244",
-          SecondaryColour: "11861244",
-          TertiaryColour: "11861244",
-          BackColour: "-2147483640",
-          Bold: "2",
-          Italic: "0",
-          BorderStyle: "2",
-          Outline: "2",
-          Shadow: "3",
-          Alignment: "1", // left, middle, right
-          MarginL: "40",
-          MarginR: "60",
-          MarginV: "40"
-        }
-      }
-
-      videoshow(images, videoOptions)
-        .audio(audio)
-        .save(video)
-        .on("start", () => {
-          console.log('> [video-robot] Starting FFmpeg')
-        })
-        .on("error", (err, stdout, stderr) => {
-          console.error("Error:", err)
-          console.error("ffmpeg stderr:", stderr)
-          reject(err)
-        })
-        .on("end", () => {
-          console.log('> [video-robot] FFmpeg closed')
-          resolve()
-        })
-    })
-  }
-
+			videoshow(images, videoOptions)
+				.audio(audio)
+				.save(video)
+				.on("start", () => {
+					console.log("> [video-robot] Starting FFmpeg");
+				})
+				.on("error", (err, stdout, stderr) => {
+					console.error("Error:", err);
+					console.error("ffmpeg stderr:", stderr);
+					reject(err);
+				})
+				.on("end", () => {
+					console.log("> [video-robot] FFmpeg closed");
+					resolve();
+				});
+		});
+	}
 }
 
-module.exports = robot
+module.exports = robot;
